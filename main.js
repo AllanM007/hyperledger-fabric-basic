e426688d169b6ca0c6a4462e4f643cf951a356dc
mzawadi <mwaranguallan345@gmail.com>
2021-07-24 18:23:12 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+/**
+ * Track the trade of a commodity from one trader to another 
+ * @param {org.mynetwork.Trade} trade - the trade to be 
+ * processed
+ * @transaction trade
+ */
+
+function tradeCommodity(trade) {
+    trade.commodity.owner = trade.newOwner;
+    return getAssetRegistry('org.acme.mynetwork.Commodity')
+    .then(function (assetRegistry) {
+        return assetRegistry.update(trade.commodity);
+    });
+}
